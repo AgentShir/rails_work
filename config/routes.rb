d45bcca461de142504154a2ed19477c5ae9e5d89
AgentShir <garciasal@gmail.com>
2022-09-15 12:28:08 -0700

    Up to 38. Many-to-Many with Checkboxes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  resources :genres
+  resources :favorites
   root "movies#index"
 
   resources :movies do
     resources :reviews
+    resources :favorites, only: [:create, :destroy]
   end
+
+  resources :users
+  get "signup" => "users#new"
+  get "signin" => "sessions#new"
+  get "signout" => "sessions#destroy"
+
+  resource :session, only: [:new, :create, :destroy]
 end
